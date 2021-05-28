8274dc46a827073e9e5bd6aa8ced7e14fd88ec1c
Aaron Haobei Song <aaron.song@faire.com>
2021-05-27 20:42:33 -0400

    bring redux

--- src/components/TaskList.stories.js
+++ src/components/TaskList.stories.js
@@ -1,27 +1,27 @@
-import React from 'react';
+import React from "react";
 
-import TaskList from './TaskList';
-import * as TaskStories from './Task.stories';
+import TaskList, { PureTaskList } from "./TaskList";
+import * as TaskStories from "./Task.stories";
 
 const component = {
-  component: TaskList,
-  title: 'TaskList',
-  decorators: [story => <div style={{ padding: '3rem' }}>{story()}</div>],
+  component: PureTaskList,
+  title: "TaskList",
+  decorators: [(story) => <div style={{ padding: "5rem" }}>{story()}</div>],
 };
 
-const Template = args => <TaskList {...args} />;
+const Template = (args) => <PureTaskList {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
   // Shaping the stories through args composition.
   // The data was inherited from the Default story in task.stories.js.
   tasks: [
-    { ...TaskStories.Default.args.task, id: '1', title: 'Task 1' },
-    { ...TaskStories.Default.args.task, id: '2', title: 'Task 2' },
-    { ...TaskStories.Default.args.task, id: '3', title: 'Task 3' },
-    { ...TaskStories.Default.args.task, id: '4', title: 'Task 4' },
-    { ...TaskStories.Default.args.task, id: '5', title: 'Task 5' },
-    { ...TaskStories.Default.args.task, id: '6', title: 'Task 6' },
+    { ...TaskStories.Default.args.task, id: "1", title: "Task 1" },
+    { ...TaskStories.Default.args.task, id: "2", title: "Task 2" },
+    { ...TaskStories.Default.args.task, id: "3", title: "Task 3" },
+    { ...TaskStories.Default.args.task, id: "4", title: "Task 4" },
+    { ...TaskStories.Default.args.task, id: "5", title: "Task 5" },
+    { ...TaskStories.Default.args.task, id: "6", title: "Task 6" },
   ],
 };
 
@@ -31,7 +31,7 @@ WithPinnedTasks.args = {
   // Inherited data coming from the Default story.
   tasks: [
     ...Default.args.tasks.slice(0, 5),
-    { id: '6', title: 'Task 6 (pinned)', state: 'TASK_PINNED' },
+    { id: "6", title: "Task 6 (pinned)", state: "TASK_PINNED" },
   ],
 };
 
