323106d3c7428a8e9f4e3b6b303f04e283c43a1d
Aaron Haobei Song <aaron.song@faire.com>
2021-05-27 21:49:30 -0400

    test ui

--- src/components/InboxScreen.js
+++ src/components/InboxScreen.js
@@ -11,7 +11,7 @@ export function PureInboxScreen({ error }) {
         <div className="wrapper-message">
           <span className="icon-face-sad" />
           <div className="title-message">Oh no!</div>
-          <div className="subtitle-message">Something went wrong</div>
+          <div className="subtitle-message">Something went horribly wrong</div>
         </div>
       </div>
     );
